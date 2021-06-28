a4418ea67544f54e4d5964e2e5458ceec172d856
vignesh956 <vigneshtulasigari2999*@gmail.com>
2021-06-28 09:12:52 +0530

    profile edit

--- src/app/auth/servieses/credentials.service.ts
+++ src/app/auth/servieses/credentials.service.ts
@@ -43,9 +43,9 @@ export class CredentialsService {
     return this.http.post('https://us-central1-etop-sign-2807.cloudfunctions.net/app/user/send-otp', email);
   }
 
-  resendOtp(payload: any){
-    console.log(payload ,  "resend otp");
-    return this.http.post('https://us-central1-etop-sign-2807.cloudfunctions.net/app/user/send-otp' , payload);
+  resendOtp(payload: any) {
+    console.log(payload, "resend otp");
+    return this.http.post('https://us-central1-etop-sign-2807.cloudfunctions.net/app/user/send-otp', payload);
   }
   verifyOtp(payload: any) {
     return this.http.post('https://us-central1-etop-sign-2807.cloudfunctions.net/app/user/verify-otp', payload);
@@ -71,6 +71,18 @@ export class CredentialsService {
 
   sendUid(uid: any) {
     return this.http.post('https://us-central1-etop-sign-2807.cloudfunctions.net/app/user/sign-in', uid);
+  }
+
+
+
+  profileDate() {
+    return {
+      name: 'T.vignesh',
+        phone: +917396989281,
+        profile:'../../assets/live-person-icon.png',
+        email : 'tulasigarivignesh1999@gmail.com'
+    };
+
   }
 }
 
--- src/app/settings-info/profile/profile.component.ts
+++ src/app/settings-info/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { CredentialsService } from 'src/app/auth/servieses/credentials.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  editUserData: boolean = false;
+  users: any = {};
 
-  constructor() { }
+  userDetailsForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    phone: new FormControl('', [
+      Validators.required,
+    ]),
+  });
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private cre: CredentialsService) { }
+
 
   ngOnInit(): void {
+    this.users = this.cre.profileDate();
+    console.log(this.users);
   }
+
+  editDetails() {
+
+    if (this.users) {
+     this.userDetailsForm.get('name')?.setValue(this.users.name);
+     this.userDetailsForm.get('phone')?.setValue(this.users.phone);
+
+    }
+    this.editUserData = true;
+  }
+  submitDetails() {}
+
+  // submitDetails() {
+
+  //   const payload = {
+  //     'name': this.userDetailsForm.value.age,
+ 
+  //   };
+  //   this.userService.editUserInformation(payload).subscribe((resp) => {
+  //     if (resp) {
+  //       this.aboutUserDetails = resp;
+  //       console.log(this.aboutUserDetails, "intrsssss")
+  //       this.editUserData = false;
+  //       if (this.introVideoFile && this.introVideoFile.length > 0) {
+  //         this.uploadIntroVideo(this.introVideoFile)
+  //       }
+  //       this.is_loading = false;
+  //     }
+  //   });
+  // }
+  cancelEdit() {
+    this.editUserData = false;
+  }
+
 
 }
