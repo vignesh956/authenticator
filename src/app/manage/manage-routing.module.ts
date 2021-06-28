071e762fa5a2550eb9fe884d8b33d331d885e8ce
saikiran <kondasaikiran1997@gmail.com>
2021-06-28 09:57:50 +0530

    manage routing  module

---
+++ src/app/manage/manage-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AchiveComponent } from '../manage-section/achive/achive.component';
+import { DraftComponent } from '../manage-section/draft/draft.component';
+import { InboxComponent } from '../manage-section/inbox/inbox.component';
+import { OutboxComponent } from '../manage-section/outbox/outbox.component';
+import { TrashComponent } from '../manage-section/trash/trash.component';
+
+const routes: Routes = [
+  {path : "achive",component:AchiveComponent},
+  {path:"draft",component:DraftComponent},
+  {path:"inbox" , component:InboxComponent},
+  {path:"outbox",component:OutboxComponent},
+  {path:"trash",component:TrashComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ManageRoutingModule { }
